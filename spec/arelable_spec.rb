010b09c791426019c91941341f1e6cbc0b5ac4b8
yu-croco <yuki.kita22@gmail.com>
2019-01-26 17:51:37 +0900

    setup minimum env

--- spec/arelable_spec.rb
+++ spec/arelable_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Arelable do
   it "has a version number" do
     expect(Arelable::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
